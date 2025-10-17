6c94d8238c076b0858afdb185714b1b55efd646f
Gaeuly <7dsgaeul@gmail.com>
2025-10-17 23:29:35 +0700

    fix: url invite

--- src/components/Invite.tsx
+++ src/components/Invite.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 
 function Invite() {
-  const discordInviteUrl = 'https://discord.com/oauth2/authorize?client_id=1151832497383030804';
+  const discordInviteUrl = 'https://discord.com/oauth2/authorize?client_id=1151832497383030804&scope=bot%20applications.commands&permissions=1099511627775';
 
   useEffect(() => {
     window.location.href = discordInviteUrl;
